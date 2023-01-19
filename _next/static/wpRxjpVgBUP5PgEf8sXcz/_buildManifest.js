5bac00130e6a6a73d7221485d3b3a238c3ea049e
ja54312 <ija54312@gmail.com>
2023-01-18 19:21:48 -0600

    Updates

---
+++ _next/static/wpRxjpVgBUP5PgEf8sXcz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-06aa6eb40e2db014.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
