6b9c2f455ba7eb346cedc63cdb3e7e6aabaee1f6
ja54312 <ija54312@gmail.com>
2023-01-18 19:08:40 -0600

    configuracion inicial

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import Prueba from '../react/components/PRUEBA/Prueba'
+
+const Home = () => {
+
+    return (
+        <>
+            <Prueba />
+        </>
+    )
+}
+
+export default Home
