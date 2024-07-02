5c4b5fa6e9a7572f6ce462df39af930004f1cc16
JA54312 <ija54312@gmail.com>
2024-07-02 13:33:13 -0600

    bannerHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 //components
 import { DeviceUnvailable } from "../components/DeviceUnaviable"
+import { BannerHome } from "../components/home/BannerHome"
 
 export default function Home() {
 
     return (
         <main>
             <DeviceUnvailable />
+            <BannerHome />
         </main>
     )
 }
---
+++ src/components/home/BannerHome/BannerHome.tsx
@@ -0,0 +1,20 @@
+//components
+import Image from 'next/image'
+//styles
+import styles from './BannerHome.module.sass'
+
+const urlBanner = '/images/banner-home-2 2.png'
+
+export const BannerHome = () => {
+    return (
+        <div className={styles.BannerHome}>
+            <div className={styles.BannerHomeContainer}>
+                <Image
+                    src={urlBanner}
+                    alt="bannerHome"
+                    fill
+                />
+            </div>
+        </div>
+    )
+}
