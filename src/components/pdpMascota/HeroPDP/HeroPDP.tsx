ff7dbba6e5dd5cefd801930bfbf5852075dbf55c
JA54312 <ija54312@gmail.com>
2024-07-09 11:55:06 -0600

    heropdp

--- src/components/pdpMascota/HeroPDP/HeroPDP.tsx
+++ src/components/pdpMascota/HeroPDP/HeroPDP.tsx
@@ -1,9 +1,47 @@
 //components
+import Image from 'next/image'
 //styles
 import styles from './HeroPDP.module.sass'
 
+const urlImage = '/photos/cuquita.png'
+const urlVideo = '/images/video.svg'
+
 export const HeroPDP = () => {
     return (
-        <section className={styles.HeroPDP}>HeroPDP</section>
+        <section className={styles.HeroPDP}>
+            <div className={styles.containerHeroPDP}>
+                <div className={styles.containerInfoName}>
+                    <div className={styles.containerName}>
+                        <span>Cuquita</span>
+                    </div>
+                    <div className={styles.containerGenre}>
+                        <span>Hembra</span>
+                    </div>
+                </div>
+                <div className={styles.containerCarrusel}>
+                    <div className={styles.containerCarruselImages}>
+                        <div className={styles.containerImage}>
+                            <Image
+                                src={urlImage}
+                                alt='imagen mascota'
+                                fill
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.containerPoints}>
+                        <span className={styles.active}></span>
+                        <span></span>
+                        <span></span>
+                        <div className={styles.containerVideo}>
+                            <Image
+                                src={urlVideo}
+                                alt='point video'
+                                fill
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
