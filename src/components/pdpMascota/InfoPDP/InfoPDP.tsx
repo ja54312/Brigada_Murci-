b78ba2c73687f56585a8ad83ca53114e98d113f4
JA54312 <ija54312@gmail.com>
2024-07-09 12:54:47 -0600

    infoPDP

--- src/components/pdpMascota/InfoPDP/InfoPDP.tsx
+++ src/components/pdpMascota/InfoPDP/InfoPDP.tsx
@@ -3,6 +3,15 @@ import styles from './InfoPDP.module.sass'
 
 export const InfoPDP = () => {
     return (
-        <section className={styles.InfoPDP}>InfoPDP</section>
+        <section className={styles.InfoPDP}>
+            <div className={styles.containerInfo}>
+                <div className={styles.containerTitle}>
+                    <span>Información:</span>
+                </div>
+                <div className={styles.containerText}>
+                    <p>Fue atacada por un arma de fuego en la cara. Convive con otros perritos, cachorros y adultos.</p>
+                </div>
+            </div>
+        </section>
     )
 }
