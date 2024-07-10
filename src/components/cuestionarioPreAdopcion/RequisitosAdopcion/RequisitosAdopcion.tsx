1e6445daba97a3b02af879a37ea5748292ff058d
JA54312 <ija54312@gmail.com>
2024-07-10 12:26:17 -0600

    requisitos cuestionario

--- src/components/cuestionarioPreAdopcion/RequisitosAdopcion/RequisitosAdopcion.tsx
+++ src/components/cuestionarioPreAdopcion/RequisitosAdopcion/RequisitosAdopcion.tsx
@@ -3,6 +3,20 @@ import styles from './RequisitosAdopcion.module.sass'
 
 export const RequisitosAdopcion = () => {
     return (
-        <section className={styles.RequisitosAdopcion}>RequisitosAdopcion</section>
+        <section className={styles.RequisitosAdopcion}>
+            <div className={styles.containerTitle}>
+                <span>Requisitos para adopción:</span>
+            </div>
+            <div className={styles.containerList}>
+                <ul>
+                    <li>Ser mayor de 21 años con solvencia económica</li>
+                    <li>Cuidar del animalito y mantenerlo en condiciones óptimas de espacio, tiempo, alimentación y ejercicio.</li>
+                    <li>Que sea un lugar cerrado y seguro para el perrito, debe ser casa propia con jardín amplio o departamento con espacio suficiente.</li>
+                </ul>
+            </div>
+            <div className={styles.containerHr}>
+                <hr />
+            </div>
+        </section>
     )
 }
