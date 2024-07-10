b672aa214227a78810aabd824e01a30bb49ded92
JA54312 <ija54312@gmail.com>
2024-07-10 12:13:18 -0600

    bannerCuestionario

--- src/components/cuestionarioPreAdopcion/BannerCuestionario/BannerCuestionario.tsx
+++ src/components/cuestionarioPreAdopcion/BannerCuestionario/BannerCuestionario.tsx
@@ -1,8 +1,25 @@
+//component
+import Image from 'next/image'
 //styles
 import styles from './BannerCuestionario.module.sass'
 
+const urlImage = '/images/BannerCuestionario.svg'
+
 export const BannerCuestionario = () => {
     return (
-        <section className={styles.BannerCuestionario}>BannerCuestionario</section>
+        <section className={styles.BannerCuestionario}>
+            <div className={styles.containerMainImage}>
+                <div className={styles.containerImage}>
+                    <Image
+                        src={urlImage}
+                        alt='banner adopciones'
+                        fill
+                    />
+                </div>
+            </div>
+            <div className={styles.containerText}>
+                <p>El objetivo de este cuestionario es encontrar la condición óptima mascota - humano, de manera que ambos sean felices tras la adopción.</p>
+            </div>
+        </section>
     )
 }
