c2a2faad1cbd7041425caa202a16bd3d4009d751
JA54312 <ija54312@gmail.com>
2024-07-10 12:48:40 -0600

    correccion gradiente fondo

--- src/components/cuestionarioPreAdopcion/FormAdopcion/FormAdopcion.tsx
+++ src/components/cuestionarioPreAdopcion/FormAdopcion/FormAdopcion.tsx
@@ -3,6 +3,17 @@ import styles from './FormAdopcion.module.sass'
 
 export const FormAdopcion = () => {
     return (
-        <section className={styles.FormAdopcion}>FormAdopcion</section>
+        <section className={styles.FormAdopcion}>
+            <form className={styles.form}>
+                <div className={styles.containerInput}>
+                    <label className={styles.label}>Correo</label>
+                    <input
+                        type='email'
+                        placeholder='Tu dirección de correo'
+                        className={styles.input}
+                    />
+                </div>
+            </form>
+        </section>
     )
 }
