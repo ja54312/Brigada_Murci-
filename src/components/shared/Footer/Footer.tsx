ed7145fb8db09ed266582eb14079ec27d7744eb3
JA54312 <ija54312@gmail.com>
2024-07-19 12:36:10 -0600

    actualizacion red tiktok

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
             <div className={styles.FooterSocialMedia}>
                 <SocialMedia srcImage='/images/FacebookLogo.png' altImage='facebook' urlSocialMedia='https://www.facebook.com/brigadamurci' />
                 <SocialMedia srcImage='/images/InstagramLogo.png' altImage='instagram' urlSocialMedia='https://www.instagram.com/brigadamurci/' />
-                <SocialMedia srcImage='/images/TiktokLogo.png' altImage='tiktok' urlSocialMedia='https://www.tiktok.com/@brigadamurcitexcoco?_t=8eBjJ6y3ZLy&_r=1' />
+                <SocialMedia srcImage='/images/TiktokLogo.png' altImage='tiktok' urlSocialMedia='https://www.tiktok.com/@brigadamurci' />
             </div>
         </footer>
     )
