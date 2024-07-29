d421bcb77836f132edf2e11688b029344cca10f2
JA54312 <ija54312@gmail.com>
2024-07-29 16:48:19 -0600

    fix adoptar home cards

--- src/components/home/AdoptarHome/AdoptarHome.tsx
+++ src/components/home/AdoptarHome/AdoptarHome.tsx
@@ -18,19 +18,19 @@ export const AdoptarHome = () => {
                 </div>
                 <div className={styles.AdoptarHomeCard}>
                     <div className={styles.TitleCard}>
-                        <span>Orgullo Mextizo de NUPEC.</span>
-                    </div>
-                    <div className={styles.TextCard}>
-                        <p>Desde 2024, formamos parte del programa Orgullo Mextizo de NUPEC, al concretar una adopción con nosotros recibirás un kit de bienvenida para tu nuevo mejor amigo.</p>
-                    </div>
-                </div>
-                <div className={styles.AdoptarHomeCard}>
-                    <div className={styles.TitleCard}>
                         <span>Acompañamiento después de la</span>
                         <span>adopción.</span>
                     </div>
                     <div className={styles.TextCard}>
                         <p>Para asegurar el bienestar del animal y la satisfacción de la nueva familia. Este proceso permite verificar que nuestros adoptables se estén adaptando adecuadamente a su nuevo hogar y que sus necesidades físicas y emocionales estén siendo atendidas.</p>
+                    </div>
+                </div>
+                <div className={styles.AdoptarHomeCard}>
+                    <div className={styles.TitleCard}>
+                        <span>Orgullo Mextizo de NUPEC.</span>
+                    </div>
+                    <div className={styles.TextCard}>
+                        <p>Desde 2024, formamos parte del programa Orgullo Mextizo de NUPEC, al concretar una adopción con nosotros recibirás un kit de bienvenida para tu nuevo mejor amigo.</p>
                     </div>
                 </div>
             </div>
